c131368853945d2100d497275631ff96862d99c5
MuhammadSiqaf <msiqaf@gmail.com>
2026-04-24 17:20:35 +0500

    Changes

---
+++ POSSystem/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace POSSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
